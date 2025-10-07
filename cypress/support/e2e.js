1c6dd37d3ebe3e76ce07b4e42c62417ac19ac6b0
Martin Middel <martin@elliat.nl>
2025-10-07 15:16:46 +0200

    chore(test): handle uncaught bogus errors from jinn-codemirror
    Something deep inside causes an error. But only in Firefox

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,3 +15,10 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands';
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    if (err.stack.includes('jinn-codemirror-bundle.js')) {
+        return false;
+    }
+    return true;
+});
